0ea0425e46d8c44c67d7cdfcf6ea3a87d8b6d608
gregaiosa <geester98@gmail.com>
2025-12-19 21:27:25 +0000

    Deploying to gh-pages from @ gregaiosa/gregaiosa.github.io@1bdbc0c389cd4068affaba9659dbec7c13542cb2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,20 +4,20 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-projects",
-    title: "projects",
+    title: "Projects",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-resume",
-          title: "resume",
+          title: "Resume",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/resume/";
           },
         },{id: "nav-about",
-          title: "about",
+          title: "About",
           description: "",
           section: "Navigation",
           handler: () => {
